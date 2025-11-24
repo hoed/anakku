78bedf6b6ca27853044873cb0a60bc3acdcf7fff
hoed <hoedhud@gmail.com>
2025-11-24 13:13:16 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="py-6 mt-12 border-t border-border/40">
       <div className="container mx-auto px-4">
         <p className="text-center text-sm text-muted-foreground flex items-center justify-center gap-2">
-          Dibuat oleh Hoed dengan <Heart className="w-4 h-4 text-red-500 fill-red-500" />
+         V2.1 Dibuat oleh Hoed dengan <Heart className="w-4 h-4 text-red-500 fill-red-500" />
         </p>
       </div>
     </footer>
